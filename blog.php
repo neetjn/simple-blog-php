76aece8390ae05388c9f11dd28ac1f9f415fbdea
neetVeritas <john@neetgroup.net>
2014-05-15 00:05:59 -0400

    Cleaned

--- blog.php
+++ blog.php
@@ -2,7 +2,7 @@
 
 class Blog {
     
-    public $post_count=0, $posts=null;
+    public $post_count, $posts;
     
     public function __construct() {
          
@@ -23,14 +23,14 @@ class Blog {
     
     public function Post($user_id, $title, $content) {
         
-        $sql = "INSERT INTO `blog_posts` (`id`, `author_id`, `post_date`, `post_title`, `post_content`, `post_comments`) VALUES (NULL, '$user_id', CURRENT_TIMESTAMP, '$title', '$content', '0')";
-        $query = mysql_query($sql);
+        $post = "INSERT INTO `blog_posts` (`id`, `author_id`, `post_date`, `post_title`, `post_content`, `post_comments`) VALUES (NULL, '$user_id', CURRENT_TIMESTAMP, '$title', '$content', '0')";
+        mysql_query($post);
     }
 }
 
 class Post {
     
-    public $data=null, $author=null;
+    public $data, $author;
     
     public function __construct($id) {
         
@@ -40,30 +40,28 @@ class Post {
     public function fetch_data($id) {
         
         $sql = "SELECT * FROM blog_posts WHERE id='$id'";
-        $query = mysql_query($sql);
-        $this->data = mysql_fetch_assoc($query);
+        $this->data = mysql_fetch_assoc( mysql_query($sql) );
         $this->author=$this->Author($this->data['author_id']);
     }
     
     private function Author($user_id) {
         
         $sql = "SELECT * FROM users WHERE id='$user_id'";
-        $query = mysql_query($sql);
-        $_user=mysql_fetch_assoc($query);
+        $_user = mysql_fetch_assoc( mysql_query($sql) );
         return $_user['username'];
     }
     
     public function Comment($user_id, $content) {
         
         $post_id = $this->data['post_id'];
-        $sql = "INSERT INTO `blog_comments` (`id`, `post_id`, `author_id`, `comment_content`) VALUES (NULL, '$post_id', '$user_id', '$content')";
-        $query = mysql_query($sql);
+        $comment = "INSERT INTO `blog_comments` (`id`, `post_id`, `author_id`, `comment_content`) VALUES (NULL, '$post_id', '$user_id', '$content')";
+        mysql_query($comment);
     }
 }
 
 class Comment {
     
-    public $data=null, $author=null;
+    public $data, $author;
     
     public function __construct($id) {
         
@@ -73,16 +71,14 @@ class Comment {
     public function fetch_data($id) {
         
         $sql = "SELECT * FROM blog_comments WHERE id='$id'";
-        $query = mysql_query($sql);
-        $this->data = mysql_fetch_assoc($query);
+        $this->data = mysql_fetch_assoc( mysql_query($sql) );
         $this->author=$this->Author($this->data['author_id']);
     }
     
     private function Author($user_id) {
         
         $sql = "SELECT * FROM users WHERE id='$user_id'";
-        $query = mysql_query($sql);
-        $_user=mysql_fetch_assoc($query);
+        $_user = mysql_fetch_assoc( mysql_query($sql) );
         return $_user['username'];
     }
 }
