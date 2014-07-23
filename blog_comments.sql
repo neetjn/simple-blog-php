a8e347ceb4f227757fee6c0c007f0d5485e08b7f
neetVeritas <john@neetgroup.net>
2014-07-23 00:16:23 -0400

    Version 1.1.2

--- blog_comments.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `blog_comments` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `post_id` bigint(20) DEFAULT NULL,
-  `author_id` bigint(20) DEFAULT NULL,
-  `comment_content` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
